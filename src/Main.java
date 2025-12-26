b4f2fb26396d7fb1e8b81e779776bf22a6132be0
Carlos Avila <avila966@gmail.com>
2025-12-26 11:25:30 -0600

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
 
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        int guess;
+        int attempts;
+        int randomNumber = random.nextInt(1, 11);
+
+
+        System.out.println(randomNumber);
+        //Number Guessing Game
     }
 }
