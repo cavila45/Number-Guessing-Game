783a56f473c9a3e3216f58ec54fd2ba2dcbc78e0
Carlos Avila <avila966@gmail.com>
2025-12-26 11:35:36 -0600

    final commit

--- src/Main.java
+++ src/Main.java
@@ -9,18 +9,33 @@ public class Main {
 
         int guess;
         int attempts = 0;
-        int randomNumber = random.nextInt(1, 11);
+        int min = 1;
+        int max = 100;
+        int randomNumber = random.nextInt(min, max + 1);
 
 
         System.out.println("Number Guessing Game");
-        System.out.println("Guess a number between 1-10: ");
+        System.out.printf("Guess a number between %d-%d\n", min, max);
 
         do{
             System.out.print("Enter a guess: ");
             guess = scanner.nextInt();
             attempts++;
+
+            if(guess < randomNumber){
+                System.out.println("TOO LOW! Try again!");
+            }
+            else if(guess > randomNumber){
+    System.out.println("TOO HIGH! Try again");
+            }
+            else{
+                System.out.println("CORRECT! The number was " + randomNumber);
+                System.out.println("# of attempts: " + attempts);
+                }
+
         }while(guess != randomNumber);
 
+            scanner.close();
         System.out.println("You have won");
     }
 }
